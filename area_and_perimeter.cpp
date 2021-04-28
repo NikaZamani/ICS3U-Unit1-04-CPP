c150b8bdc32053b52eb96b1355a9cb8554c1f3e9
Nika Zamani <nika.zamani@mths.ca>
2021-04-28 04:20:48 +0000

    added area_and_perimeter.cpp

---
+++ area_and_perimeter.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Nika Zamani All rights reserved
+//
+// Created by Nika Zamani
+// Created on April 2021
+// This program calculates the area and perimeter of a rectangle
+//    with dimensions 5cm × 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "5cm × 3cm" << std::endl;
+    std::cout << ""<< std::endl;
+    std::cout << "Area is: " << (5*3) << "cm^2" << std::endl;
+    std::cout << "Perimeter is: " << (2*(5+3)) << "cm" << std::endl;
+}
---
+++ basic_math.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 Nika Zamani All rights reserved
+//
+// Created by Nika Zamani
+// Created on April 2021
+// This program does basic math
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+	std::cout << "9+2=" << (9+2) << std::endl;
+	std::cout << "7-2=" << (7-2) << std::endl;
+	std::cout << "4/2=" << (4/2) << std::endl;
+	std::cout << "4+4/2=" << (4+4/2) << std::endl;
+	std::cout << "3+4*2=" << (3+4*2) << std::endl;
+	std::cout << "5+2^3=" << (5+2^3) << std::endl;
+	std::cout << "5+pow(2,3)=" << (5+pow(2,3)) << std::endl;
+}
